ddfa66c47dcb719bb65738f318b7dbe56cdfd7e9
abid testng <abid@a.com>
2018-01-11 08:32:06 +0530

    deletenode

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,3 +1,5 @@
+package linkedlist;
+
 // A complete working Java program to demonstrate all insertion methods
 // on linked list
 public class LinkedList {
@@ -85,6 +87,37 @@ public class LinkedList {
         }
     }
 
+    /* Given a reference (pointer to pointer) to the head of a list
+       and a position, deletes the node at the given position */
+    void deleteNode(int position)
+    {
+        // If linked list is empty
+        if (head == null)
+            return;
+
+        // Store head node
+        Node temp = head;
+
+        // If head needs to be removed
+        if (position == 0)
+        {
+            head = head.next;   // Change head
+            return;
+        }
+
+        // Find previous node of the node to be deleted
+        for (int i=0; temp!=null && i<position-1; i++)
+            temp = temp.next;
+
+        // If position is more than number of ndoes
+        if (temp == null || temp.next == null)
+            return;
+
+        // Node temp->next is the node to be deleted
+        // Store pointer to the next of node to be deleted
+        temp.next = temp.next.next; // Unlink the deleted node from list
+    }
+
     /* Driver program to test above functions. Ideally this function
        should be in a separate user class.  It is kept here to keep
        code compact */
@@ -113,5 +146,12 @@ public class LinkedList {
 
         System.out.println("\nCreated Linked list is: ");
         llist.printList();
+
+        llist.deleteNode(4);  // Delete node at position 4
+
+        System.out.println("\nLinked List after Deletion at position 4: ");
+        llist.printList();
+
+
     }
 }
